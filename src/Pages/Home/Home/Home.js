2d8ba1f5acdd2310a021ef15b1b8a8540b528908
sauravmondal24 <sauravmondal321@gmail.com>
2022-11-24 12:46:30 +0600

    Banner section done

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import OurBrand from '../OurBrand/OurBrand';
 
 const Home = () => {
 	return (
 		<div>
 			<Banner></Banner>
+			<OurBrand></OurBrand>
 		</div>
 	);
 };
---
+++ src/Pages/Home/OurBrand/OurBrand.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import microsoft from '../../../assets/brand/Microsoft-Logo-40y-1.jpg';
+import dell from '../../../assets/brand/dell-technologies-logo-40y-1.jpg';
+import hp from '../../../assets/brand/MicrosoftTeams-image-1.jpg';
+// import lenovo from '../../../assets/brand/Lenovo_logo_40y-1';
+
+const OurBrand = () => {
+	return (
+		<div className="py-12 rounded shadow-2xl px-12 border">
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 justify-between">
+				<div>
+					<img src={microsoft} alt="" />
+				</div>
+				<div>
+					<img src={dell} alt="" />
+				</div>
+				<div>
+					<img src={hp} alt="" />
+				</div>
+				<div>
+					<img src={dell} alt="" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default OurBrand;
